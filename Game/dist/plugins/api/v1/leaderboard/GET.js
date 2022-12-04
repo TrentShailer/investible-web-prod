03cd33ae75f5f3293ed0f60bea7c95e4c2f4d53b
Trent Shailer <trent.shailer@gmail.com>
2022-12-04 20:48:36 +1300

    leaderboard pagination

--- Game/dist/plugins/api/v1/leaderboard/GET.js
+++ Game/dist/plugins/api/v1/leaderboard/GET.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 async function default_1(fastify) {
     fastify.get("/", async (request, reply) => {
+        let page = request.query.page ? request.query.page : 1;
         // get if there is a current competition
         const { rows: competitionRows } = await fastify.pg.query("SELECT id, start_date, end_date FROM competition WHERE end_date > NOW() AND start_date < NOW() ORDER BY start_date ASC LIMIT 1;");
         if (competitionRows.length === 0) {
@@ -16,8 +17,12 @@ async function default_1(fastify) {
 					INNER JOIN player ON leaderboard.player_id = player.id
 					ORDER BY leaderboard.player_id DESC, game.portfolio_value DESC)t
 				ORDER BY portfolio_value DESC
-				LIMIT 10`);
-            return reply.status(200).send(rows);
+				LIMIT 10 OFFSET $1;`, [(page - 1) * 10]);
+            // get the total number of players
+            const { rows: totalRows } = await fastify.pg.query("SELECT COUNT(DISTINCT player_id) FROM leaderboard;");
+            const count = totalRows[0].count;
+            const pageCount = Math.ceil(count / 10);
+            return reply.status(200).send({ leaderboard: rows, pageCount });
         }
         else {
             const competition = competitionRows[0];
@@ -32,8 +37,14 @@ async function default_1(fastify) {
 					WHERE game.timestamp BETWEEN $1 AND $2
 					ORDER BY leaderboard.player_id DESC, game.portfolio_value DESC)t
 				ORDER BY portfolio_value DESC
-				LIMIT 10`, [competition.start_date, competition.end_date]);
-            return reply.status(200).send(rows);
+				LIMIT 10 OFFSET $3;`, [competition.start_date, competition.end_date, (page - 1) * 10]);
+            // get the total number of players
+            const { rows: totalRows } = await fastify.pg.query(`SELECT COUNT(DISTINCT player_id) FROM leaderboard
+				INNER JOIN game ON leaderboard.game_id = game.id
+				WHERE game.timestamp BETWEEN $1 AND $2;`, [competition.start_date, competition.end_date]);
+            const count = totalRows[0].count;
+            const pageCount = Math.ceil(count / 10);
+            return reply.status(200).send({ leaderboard: rows, pageCount });
         }
     });
 }
